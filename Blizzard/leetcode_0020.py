dd09257130fc9121fb3abad312e336361f03fa53
numdar335 <56404005+numdar335@users.noreply.github.com>
2021-04-21 04:45:42 +0300

    Create leetcode_0020.py

---
+++ Blizzard/leetcode_0020.py
@@ -0,0 +1,11 @@
+class Solution(object):
+  def isValid(self,s):
+    helper = []
+    for i in s:
+      if i in ['(','{','[']: helper.append(i)
+      elif helper:
+        temp = helper.pop()
+        if (temp != '(' and i == ')') or (temp != '{' and i == '}') or (temp != '[' and i == ']'): return False
+      else: return False
+    if not helper: return True
+    return False
