a8c46c9608c06fecac4319c4425af140530c914d
Nikos Karellas <56404005+numdar335@users.noreply.github.com>
2021-04-22 16:20:15 +0300

    Update codesignal_containsDuplicates.cpp

--- Palantir/codesignal_containsDuplicates.cpp
+++ Palantir/codesignal_containsDuplicates.cpp
@@ -1,8 +1,8 @@
-//task description: https://app.codesignal.com/interview-practice/task/CfknJzPmdbstXhsoJ/description
-bool containsDuplicates(vector<int> a) {
-  sort(a.begin(),a.end());
-  int n = a.size();
-  for (int i = 2; i <= n; ++i)
-    if (a[i-1] == a[i-2]) return true;
+// task description: https://app.codesignal.com/interview-practice/task/CfknJzPmdbstXhsoJ/description
+bool containsDuplicates(vector<int> A) {
+  sort(A.begin(),A.end());
+  int n = A.size();
+  for (int i = 1; i < n; ++i)
+    if (A[i] == A[i-1]) return true;
   return false;
 }
