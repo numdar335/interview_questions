f76832a67cedab509b0f030b8954aad1bfbd6f4c
Nikos Karellas <56404005+numdar335@users.noreply.github.com>
2021-04-22 04:22:05 +0300

    Update leetcode_0001.py

--- Snapchat/leetcode_0001.py
+++ Snapchat/leetcode_0001.py
@@ -1,11 +1,11 @@
 #task description: https://leetcode.com/problems/two-sum/
 class Solution:
-  def twoSum(self, nums: List[int], target: int) -> List[int]:
-    A = []
+  def twoSum(self,nums: List[int],target: int) -> List[int]:
+    Arr = []
     n = len(nums)
-    for i in range(1,n+1):
-      for j in range(i+1,n+1):
-        if nums[i-1]+nums[j-1] == target:
-          A.append(i-1)
-          A.append(j-1)
-          return A
+    for i in range(n):
+      for j in range(i+1,n):
+        if nums[i]+nums[j] == target:
+          Arr.append(i)
+          Arr.append(j)
+          return Arr
