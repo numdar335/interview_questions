552b657e1964cc61484dd5110046939cd7c7de72
Nikos Karellas <56404005+numdar335@users.noreply.github.com>
2021-04-22 05:18:47 +0300

    Update leetcode_0118.py

--- Facebook/leetcode_0118.py
+++ Facebook/leetcode_0118.py
@@ -1,11 +1,11 @@
-#task description: https://leetcode.com/problems/pascals-triangle/
+# task description: https://leetcode.com/problems/pascals-triangle/
 class Solution:
-  def generate(self, numRows: int):
+  def generate(self,numRows: int):
     A = []
-    for i in range(1,numRows+1):
+    for i in range(numRows):
       B = []
-      for j in range(1,i+1):
-        if j != i and j != 1: B.append(A[i-2][j-2]+A[i-2][j-1])
+      for j in range(i+1):
+        if j not in [0,i]: B.append(A[i-1][j-1]+A[i-1][j])
         else: B.append(1)
       A.append(B)
     return A
