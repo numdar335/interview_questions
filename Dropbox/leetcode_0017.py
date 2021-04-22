881b0caa267a14d1aa35a4065d7b0bf1378b8e79
Nikos Karellas <56404005+numdar335@users.noreply.github.com>
2021-04-22 16:25:57 +0300

    Update leetcode_0017.py

--- Dropbox/leetcode_0017.py
+++ Dropbox/leetcode_0017.py
@@ -2,9 +2,9 @@
 class Solution(object):
   def letterCombinations(self,digits):
     if not digits: return digits
-    A, B, C, D = [['a','b','c',''],['d','e','f',''],['g','h','i',''],['j','k','l',''],['m','n','o',''],['p','q','r','s'],['t','u','v',''],['w','x','y','z'],['','','','']], [3,3,3,3,3,4,3,4,1], [int(digits[0])-2,8,8,8], []
+    n, A, B, C, D = len(digits), [['a','b','c',''],['d','e','f',''],['g','h','i',''],['j','k','l',''],['m','n','o',''],['p','q','r','s'],['t','u','v',''],['w','x','y','z'],['','','','']], [3,3,3,3,3,4,3,4,1], [int(digits[0])-2,8,8,8], []
     for i in range(B[C[0]]):
-      for x in range(1,len(digits)): C[x] = int(digits[x])-2
+      for x in range(1,n): C[x] = int(digits[x])-2
       for j in range(B[C[1]]):
         for k in range(B[C[2]]):
           for l in range(B[C[3]]):
