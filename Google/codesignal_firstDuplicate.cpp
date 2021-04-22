1404d7978a526f62655200e277823f568f358302
Nikos Karellas <56404005+numdar335@users.noreply.github.com>
2021-04-22 16:16:21 +0300

    Update codesignal_firstDuplicate.cpp

--- Google/codesignal_firstDuplicate.cpp
+++ Google/codesignal_firstDuplicate.cpp
@@ -1,10 +1,10 @@
 //task description: https://app.codesignal.com/interview-practice/task/pMvymcahZ8dY4g75q/description
-int firstDuplicate(vector<int> a) {
-  vector<int> b(99999);
-  fill(b.begin(),b.end(),0);
-  b[a[0]] = 1;
-  int n = a.size();
-  for (int i = 2; i <= n; ++i)
-    if (++b[a[i-1]] > 1) return a[i-1];
+int firstDuplicate(vector<int> A) {
+  vector<int> B(99999);
+  fill(B.begin(),B.end(),0);
+  B[A[0]] = 1;
+  int n = A.size();
+  for (int i = 1; i < n; ++i)
+    if (++B[A[i]] > 1) return A[i];
   return -1;
 }
