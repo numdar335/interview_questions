c18bd7f39a70e2860228f74c044b43ab529339d5
Nikos Karellas <56404005+numdar335@users.noreply.github.com>
2021-04-22 05:20:45 +0300

    Update leetcode_0387.cpp

--- Google/leetcode_0387.cpp
+++ Google/leetcode_0387.cpp
@@ -1,13 +1,13 @@
-//task description: https://leetcode.com/problems/first-unique-character-in-a-string/
+// task description: https://leetcode.com/problems/first-unique-character-in-a-string/
 class Solution {
 public:
   int firstUniqChar(string s) {
-    vector<int> A(26);
-    fill(A.begin(),A.end(),0);
-    int n = s.size();
-    for (int i = 1; i <= n; ++i) ++A[s.at(i-1)-97];
-    for (int i = 1; i <= n; ++i)
-     if (A[s.at(i-1)-97] == 1) return i-1;
+    vector<int> Arr(26);
+    fill(Arr.begin(),Arr.end(),0);
+    int i, n = s.size();
+    for (i = 0; i < n; ++i) ++Arr[s.at(i)-97];
+    for (i = 0; i < n; ++i)
+     if (Arr[s.at(i)-97] == 1) return i;
     return -1;
   }
 };
