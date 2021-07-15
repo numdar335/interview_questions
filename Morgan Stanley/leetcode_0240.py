9f81f52764bcf541ac33f3e277021a23dd02b77f
Nikos Karellas <56404005+numdar335@users.noreply.github.com>
2021-07-15 04:51:32 +0300

    Create leetcode_0240.py

---
+++ Morgan Stanley/leetcode_0240.py
@@ -0,0 +1,9 @@
+# task description: https://leetcode.com/problems/search-a-2d-matrix-ii/
+class Solution(object):
+  def searchMatrix(self,matrix,target):
+    i, j = len(matrix)-1, 0
+    while i >= 0 and j <= len(matrix[0])-1:
+      if target > matrix[i][j]: j += 1
+      elif target < matrix[i][j]: i -= 1
+      else: return True
+    return False
