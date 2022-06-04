266dd4eb5ca2892a6ded47bde10044e882a3ce3d
numdar335 <nikoskarel@gmail.com>
2022-06-04 20:35:24 +0300

    Delete codesignal_containsDuplicates.cpp

--- Palantir/codesignal_containsDuplicates.cpp
+++
@@ -1,8 +0,0 @@
-// task description: https://app.codesignal.com/interview-practice/task/CfknJzPmdbstXhsoJ/description
-bool containsDuplicates(vector<int> A) {
-  sort(A.begin(),A.end());
-  int n = A.size();
-  for (int i = 1; i < n; ++i)
-    if (A[i] == A[i-1]) return true;
-  return false;
-}
